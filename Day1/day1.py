50067c9d1ae861d6f8bf8c9e01f811dc4b330e13
granaj91 <32178706+granaj91@users.noreply.github.com>
2020-12-05 18:00:23 -0500

    Optimized part 1 of day1.py

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,3 +1,4 @@
+import math
 file = open("input.txt", "r")
 report = []
 
@@ -5,11 +6,16 @@ for num in file:
     report.append(int(num))
 
 # Report Repair: Part 1
-for i in range(len(report)):
-    for j in range(i, len(report)):
-        if report[i] + report[j] == 2020:
-            print(report[i] * report[j])
-            break
+report.sort()
+i = 0
+j = len(report) - 1
+while i < j:
+    while(report[i] + report[j] > 2020):
+        j -= 1
+    if report[i] + report[j] == 2020:
+        print(report[i] * report[j])
+        break
+    i += 1
 
 # Report Repair: Part 2
 for i in range(len(report)):
