798defe49f69a29a902e3b16e7c0fb053ff57228
granaj91 <32178706+granaj91@users.noreply.github.com>
2020-12-05 15:24:45 -0500

    Finished advent of code day 5

---
+++ Day5/day5.py
@@ -0,0 +1,41 @@
+import math
+
+def findSeat(boardpass: str, frontHalf: int, backHalf: int) -> int:
+    if len(boardpass) == 0:
+        return frontHalf
+    
+    if boardpass[0] == 'F' or boardpass[0] == 'L':
+        return findSeat(boardpass[1:], frontHalf, math.ceil((backHalf - frontHalf)/2 + frontHalf))
+    else:
+        return findSeat(boardpass[1:], math.ceil((backHalf - frontHalf)/2 + frontHalf), backHalf)
+
+      
+# Binary Boarding: Part 1
+maxID = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        row = findSeat(line[:7], 0, 127)
+        col = findSeat(line[7:].rstrip(), 0, 7)
+        passID = row * 8 + col
+
+        if passID > maxID:
+           maxID = passID
+
+print("Max. boarding pass ID: " + str(maxID))
+
+# Binary Boarding: Part 2
+boardingpassList = [0 for i in range(maxID + 1)]
+with open("input.txt", "r") as file:
+    for line in file:
+        row = findSeat(line[:7], 0, 127)
+        col = findSeat(line[7:].rstrip(), 0, 7)
+        passID = row * 8 + col
+
+        boardingpassList[passID] = passID
+
+i = 0
+while(boardingpassList[i] == 0):
+    i += 1
+
+del boardingpassList[:i]
+print("My boarding pass ID: " + str(boardingpassList.index(0) + i))
