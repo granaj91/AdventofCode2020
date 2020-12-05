3af966e9c0317401415f926de922ac2217564ff0
granaj91 <32178706+granaj91@users.noreply.github.com>
2020-12-05 16:34:42 -0500

    Refactored day3.py

--- Day3/day3.py
+++ Day3/day3.py
@@ -3,13 +3,21 @@ with open("input.txt", "r") as file:
     for line in file:
         tobogganMap.append(line.rstrip())
 
-trees = 0
-j = 3
-for i in range(1, len(tobogganMap)):
-    if(j >= len(tobogganMap[i])):
-        j = j - (len(tobogganMap[i]))
-    if(j < len(tobogganMap[i]) and tobogganMap[i][j] == '#'):
-        trees += 1
-    j += 3
+
+def countTrees(right: int, down: int) -> int:
+    trees = 0
+    j = right
+    for i in range(down, len(tobogganMap), down):
+        if(j >= len(tobogganMap[i])):
+            j = j - (len(tobogganMap[i]))
+        if(j < len(tobogganMap[i]) and tobogganMap[i][j] == '#'):
+            trees += 1
+        j += right
     
-print(trees)
+    return trees
+
+# Toboggan Trajectory: Part 1
+print(countTrees(3,1))
+
+# Toboggan Trajectory: Part 2
+print(str(countTrees(1,1) * countTrees(3,1) * countTrees(5,1) * countTrees(7,1) * countTrees(1,2)))
